c8c8b338d691f0037e54b596a0824936083c50cb
Wellici Araujo <wja1@ifal.edu.br>
2019-07-28 23:31:56 -0300

    layout definido, consultando da api

--- js/api.js
+++ js/api.js
@@ -1,14 +1,34 @@
 const baseUrl = "http://fipeapi.appspot.com/api/1/carros";
 
+const selectMarca = document.querySelector("#selectMarca");
+
 const listaModelos = document.querySelector("#lista-modelos");
 
-fetch(baseUrl + "/veiculos/21.json")
+const selectModelo = document.querySelector("#selectModelo");
+
+fetch("http://fipeapi.appspot.com/api/1/carros/marcas.json")
   .then(data => data.json())
   .then(ArrayData => {
     ArrayData.map(data => {
-      let li = document.createElement("li");
-      li.innerHTML = data.name;
-      listaModelos.appendChild(li);
+      let option = document.createElement("option");
+      option.value = data.id;
+      option.innerHTML = data.name;
+      selectMarca.appendChild(option);
       console.log(data);
     });
   });
+
+selectMarca.addEventListener("change", event => {
+  console.log(selectMarca.value);
+  fetch(`${baseUrl}/veiculos/${selectMarca.value}.json`)
+    .then(data => data.json())
+    .then(ArrayData => {
+      selectModelo.innerText = "";
+      ArrayData.map(data => {
+        let option = document.createElement("option");
+        option.innerHTML = data.name;
+        selectModelo.appendChild(option);
+        console.log(selectModelo);
+      });
+    });
+});
