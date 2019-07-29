d534d77ef0d58b290313ce65838c2113a24a3fcd
Wellici Araujo <wja1@ifal.edu.br>
2019-07-29 15:40:06 -0300

    bootstrap adicionado

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 const baseUrl = "http://fipeapi.appspot.com/api/1/carros";
-
 const selectMarca = document.querySelector("#selectMarca");
-
 const listaModelos = document.querySelector("#lista-modelos");
-
 const selectModelo = document.querySelector("#selectModelo");
+const selectAno = document.querySelector("#selectAno");
 
-fetch("http://fipeapi.appspot.com/api/1/carros/marcas.json")
+let marca, modelo;
+
+fetch(`${baseUrl}/marcas.json`)
   .then(data => data.json())
   .then(ArrayData => {
     ArrayData.map(data => {
@@ -14,21 +14,38 @@ fetch("http://fipeapi.appspot.com/api/1/carros/marcas.json")
       option.value = data.id;
       option.innerHTML = data.name;
       selectMarca.appendChild(option);
-      console.log(data);
     });
   });
 
 selectMarca.addEventListener("change", event => {
-  console.log(selectMarca.value);
-  fetch(`${baseUrl}/veiculos/${selectMarca.value}.json`)
+  marca = selectMarca.value;
+  fetch(`${baseUrl}/veiculos/${marca}.json`)
     .then(data => data.json())
     .then(ArrayData => {
       selectModelo.innerText = "";
       ArrayData.map(data => {
         let option = document.createElement("option");
+        option.value = data.id;
         option.innerHTML = data.name;
         selectModelo.appendChild(option);
-        console.log(selectModelo);
       });
     });
+});
+
+selectModelo.addEventListener("change", () => {
+  modelo = selectModelo.value;
+  fetch(`${baseUrl}/veiculo/${marca}/${modelo}.json`)
+    .then(data => data.json())
+    .then(ArrayData => {
+      selectAno.innerText = "";
+      ArrayData.map(data => {
+        let option = document.createElement("option");
+        option.innerHTML = data.fipe_codigo;
+        selectAno.appendChild(option);
+      });
+    });
+});
+
+selectAno.addEventListener("change", () => {
+  fetch(`${baseUrl}/veiculo/${selectMarca.value}/4828/2013-1.json`);
 });
